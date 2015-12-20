3547bdf1d7cedaa9625355353d14b1c29edc2af5
Andreas Hackl <andreas.hackl@johanniter.at>
2015-12-19 21:41:18 -0800

    vec3 test code

--- raytracer.cpp
+++ raytracer.cpp
@@ -1,6 +1,12 @@
 #include "PPMWriter.h"
 
+#include "vec3.h"
+
 int main()
 {
+    raymond::vec3<float> a(1.0f,0.0f, 0.0f), b(0.0f,1.0f, 0.0f);
+    raymond::vec3<float> cross = a.cross(b);
+    raymond::vec3<float> add = cross + a;
+    add.setLength(10.0f);
 	return 0;
 }
