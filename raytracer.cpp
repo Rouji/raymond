d5fb84853b3b1072a36b6c4bd2405afd5d04b17c
Andreas Hackl <andreas.hackl@johanniter.at>
2015-12-20 10:12:02 -0800

    sphere intersect test

--- raytracer.cpp
+++ raytracer.cpp
@@ -2,20 +2,31 @@
 
 #include "vec3.h"
 #include "mat4.h"
+#include "Sphere.h"
 
 using namespace raymond;
 
 int main()
 {
-    vec3f a(1.0f,0.0f, 0.0f), b(0.0f,2.5f, 0.0f);
-    mat4f m,n,o;
-    m.setTranslation(vec3f(5.0f, 3.0f, 0.0f));
-    n.setScale(vec3f(2.0f, 0.5f, 1.0f));
-    vec3f translated = m.transform(a);
-    vec3f combined = n.transform(translated);
-    n *= m;
-    vec3f matprodtest = n.transform(a);
-    
+    io::PPMWriter out(500, 500);
 
+    vec3f o(0.0f,0.0f, 0.0f), d(0.0f,0.0f, 1.0f);
+    Sphere s;
+    s.setPosition(vec3f(0.0f, 0.0f, 1000.0f));
+    s.setRadius(250.0f);
+
+    float inter;
+    for (int i = 0; i < 500; i++)
+    {
+        for (int j = 0; j < 500; j++)
+        {
+            if (s.intersect(o + vec3f((float)(i-250), (float)(j - 250), 0.0f), d, &inter))
+                out.SetPixel(i, j, (unsigned char)inter-750, (unsigned char)inter - 750, (unsigned char)inter - 750);
+            else
+                out.SetPixel(i, j, 0, 0, 0);
+        }
+    }
+
+    out.Write("test.ppm");
 	return 0;
 }
