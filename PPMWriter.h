766339c30ab406cdabdbaae17cdcf182e0e3d1af
Andreas Hackl <andreas.hackl@johanniter.at>
2015-12-20 17:26:36 -0800

    fixed cut off comment

--- PPMWriter.h
+++ PPMWriter.h
@@ -5,8 +5,10 @@ public:
     PPMWriter(unsigned int width, unsigned int height);
     ~PPMWriter();
 
-	//allocates 
+	//allocates a block of memory for width*heigth pixels
+    //call first before doing anything else
     void Alloc(unsigned int width, unsigned int height);
+
     unsigned int SetPixel(unsigned int x, unsigned int y, unsigned char r, unsigned char g, unsigned char b);
     unsigned int Write(const char* path);
 
