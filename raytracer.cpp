d84111c42d9cb76260425785fb1747a103db1968
Andreas Hackl <andreas.hackl@johanniter.at>
2015-12-21 10:17:45 -0800

    scene loader test code

--- raytracer.cpp
+++ raytracer.cpp
@@ -13,7 +13,7 @@ int main()
     /*io::CPPMImageWriter out(dim2i(500, 400));
 
     vec3f eye(0.0f,0.0f, 0.0f), lookat(0.0f,0.0f, 1.0f), up(0.0f,1.0f,0.0f);
-    scene::Sphere s;
+    scene::CSphere s;
     s.setPosition(vec3f(0.0f, 0.0f, 800.0f));
     s.setRadius(250.0f);
 
@@ -40,6 +40,7 @@ int main()
 
     scene::CSceneLoader loader;
     scene::CScene* pScene = loader.loadXML("scenes/example2.xml");
+    pScene->render();
 
     delete pScene;
 	return 0;
