df0ff39eeac1f7b2cc59f62e3d62a81b08839062
Andreas Hackl <andreas.hackl@johanniter.at>
2015-12-21 09:07:38 -0800

    sceneloader test code

--- raytracer.cpp
+++ raytracer.cpp
@@ -4,38 +4,43 @@
 #include "mat4.h"
 #include "CSphere.h"
 #include "CCamera.h"
+#include "CSceneLoader.h"
 
 using namespace raymond;
 
 int main()
 {
-    io::CPPMImageWriter out(500, 400);
+    /*io::CPPMImageWriter out(dim2i(500, 400));
 
     vec3f eye(0.0f,0.0f, 0.0f), lookat(0.0f,0.0f, 1.0f), up(0.0f,1.0f,0.0f);
-    Sphere s;
+    scene::Sphere s;
     s.setPosition(vec3f(0.0f, 0.0f, 800.0f));
     s.setRadius(250.0f);
 
-    CCamera c(eye,
+    scene::CCamera c(eye,
         lookat,
         up,
-        500,
-        400);
+        dim2i(500,400));
 
-    SIntersection inter;
+    scene::SIntersection inter;
     for (int i = 0; i < 500; i++)
     {
         for (int j = 0; j < 400; j++)
         {
-            if (s.intersect(c.getRay(i, 400 - j), &inter))
+            if (s.intersect(c.getRay(vec2i(i, 400 - j)), &inter))
             {
-                out.SetPixel(i, j, (inter.Normal.X + 1.0f) * 125, (inter.Normal.Y + 1.0f) * 125, (inter.Normal.Z + 1.0f) * 125);
+                out.SetPixel(vec2i(i, j), col4f((inter.Normal + 1.0f)/2.0f));
             }
             else
-                out.SetPixel(i, j, 0, 0, 0);
+                out.SetPixel(vec2i(i, j), col4f());
         }
     }
 
-    out.Write("test.ppm");
+    out.Write("test.ppm");*/
+
+    scene::CSceneLoader loader;
+    scene::CScene* pScene = loader.loadXML("scenes/example2.xml");
+
+    delete pScene;
 	return 0;
 }
