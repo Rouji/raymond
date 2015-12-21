306770b55da4d960ee976e6e9fc0a545e892bcd8
Andreas Hackl <andreas.hackl@johanniter.at>
2015-12-21 09:06:18 -0800

    added setters/getters to CCamera, made it recompute the plane only when needed

--- CCamera.cpp
+++ CCamera.cpp
@@ -5,32 +5,111 @@ namespace raymond
 namespace scene
 {
 
-CCamera::CCamera(const vec3f & eye, 
-                 const vec3f & lookAt, 
-                 const vec3f & up, 
-                 u32 resX, 
-                 u32 resY, 
-                 f32 fov)
+CCamera::CCamera(const vec3f& eye, 
+                 const vec3f& lookAt, 
+                 const vec3f& up, 
+                 const dim2i& size,
+                 f32 fov,
+                 u32 maxBounces) :
+    m_eye(eye),
+    m_lookAt(lookAt),
+    m_up(up),
+    m_imageSize(size),
+    m_fov(fov),
+    m_maxBounces(maxBounces),
+    m_dirty(true)
+{}
+
+ray3f CCamera::getRay(const vec2i& pos)
 {
-    m_eye = eye;
-    f32 aspect = (f32)resY / (f32)resX;
-    vec3f viewDir = lookAt - eye;
-    vec3f u = viewDir.cross(up).normalise();
+    if (m_dirty)
+        computePlane();
+
+    return ray3f(m_eye, m_viewPlaneCorner + (m_vecPerPixelX * pos.X) + (m_vecPerPixelY * pos.Y));
+}
+
+void CCamera::setEyePoint(const vec3f & eye)
+{
+    m_eye = eye; 
+    m_dirty = true;
+}
+
+void CCamera::setLookAtPoint(const vec3f & lookAt)
+{
+    m_lookAt = lookAt;
+    m_dirty = true;
+}
+
+void CCamera::setUpVector(const vec3f & up)
+{
+    m_up = up;
+    m_dirty = true;
+}
+
+void CCamera::setImageSize(const dim2i & size)
+{
+    m_imageSize = size;
+    m_dirty = true;
+}
+
+void CCamera::setFOV(f32 fov)
+{
+    m_fov = fov;
+    m_dirty = true;
+}
+
+void CCamera::setMaxBounces(u32 maxBounces)
+{
+    m_maxBounces = maxBounces;
+    m_dirty = true;
+}
+
+const vec3f & CCamera::getEyePoint() const
+{
+    return m_eye;
+}
+
+const vec3f & CCamera::getLookAtPoint() const
+{
+    return m_lookAt;
+}
+
+const vec3f & CCamera::getUpVector() const
+{
+    return m_up;
+}
+
+const dim2i & CCamera::getImageSize() const
+{
+    return m_imageSize;
+}
+
+f32 CCamera::getFOV() const
+{
+    return m_fov;
+}
+
+u32 CCamera::getMaxBounces() const
+{
+    return m_maxBounces;
+}
+
+void CCamera::computePlane()
+{
+    f32 aspect = (f32)m_imageSize.Y / (f32)m_imageSize.X;
+    vec3f viewDir = m_lookAt - m_eye;
+    vec3f u = viewDir.cross(m_up).normalise();
     vec3f v = u.cross(viewDir).normalise();
 
     //actually half of the viewplane width/heigth
-    f32 viewPlaneW = maths::tangent(fov / 2.0f);
+    f32 viewPlaneW = maths::tangent(m_fov / 2.0f);
     f32 viewPlaneH = viewPlaneW*aspect;
 
-    m_viewPlaneCorner = lookAt - (u*viewPlaneW) - (v*viewPlaneH);
+    m_viewPlaneCorner = m_lookAt - (u*viewPlaneW) - (v*viewPlaneH);
 
-    m_vecPerPixelX = (u*viewPlaneW * 2) / resX;
-    m_vecPerPixelY = (v*viewPlaneH * 2) / resY;
-}
-
-ray3f CCamera::getRay(u32 pixelX, u32 pixelY)
-{
-    return ray3f(m_eye, m_viewPlaneCorner + (m_vecPerPixelX * pixelX) + (m_vecPerPixelY * pixelY));
+    m_vecPerPixelX = (u*viewPlaneW * 2) / m_imageSize.X;
+    m_vecPerPixelY = (v*viewPlaneH * 2) / m_imageSize.Y;
+    m_dirty = false;
 }
 
 } //scene
--- CCamera.h
+++ CCamera.h
@@ -4,6 +4,7 @@
 #include "ray3.h"
 #include "mat4.h"
 #include "vec3.h"
+#include "vec2.h"
 #include "maths.h"
 
 namespace raymond
@@ -15,26 +16,44 @@ class CCamera
 {
 public:
     //fov assumed to be horizontal
-    CCamera(const vec3f& eye = vec3f(0), 
-            const vec3f& lookAt = vec3f(1.0f, 0, 0), 
-            const vec3f& up = vec3f(0, 1.0f, 0), 
-            u32 resX = 512, 
-            u32 resY = 512, 
-            f32 fov = PI / 2.0f);
+    CCamera(const vec3f& eye = vec3f(0.0f), 
+            const vec3f& lookAt = vec3f(1.0f, 0.0f, 0.0f), 
+            const vec3f& up = vec3f(0.0f, 1.0f, 0.0f), 
+            const dim2i& size = dim2i(512,512), 
+            f32 fov = PI / 2.0f,
+            u32 maxBounces = 5);
 
     //return a ray for a given screen pixel
-    ray3f getRay(unsigned int pixelX, unsigned int pixelY);
+    ray3f getRay(const vec2i& pos);
+
+    void setEyePoint(const vec3f& eye);
+    void setLookAtPoint(const vec3f& lookAt);
+    void setUpVector(const vec3f& up);
+    void setImageSize(const dim2i& size);
+    void setFOV(f32 fov);
+    void setMaxBounces(u32 maxBounces);
+
+    const vec3f& getEyePoint() const;
+    const vec3f& getLookAtPoint() const;
+    const vec3f& getUpVector() const;
+    const dim2i& getImageSize() const;
+    f32 getFOV() const;
+    u32 getMaxBounces() const;
 
 private:
+    //(re)compute parameters needed form pixel rays
+    void computePlane();
+    bool m_dirty; //true -> needs to be recomputed
+
     vec3f m_eye;
     vec3f m_lookAt;
     vec3f m_up;
-    u32 m_resX;
-    u32 m_resY;
+    dim2i m_imageSize;
     f32 m_fov;
     vec3f m_viewPlaneCorner; //bottom-left corner of view plane
     vec3f m_vecPerPixelX; //vector to add to a point on the view plane to get to ne next pixel along x
     vec3f m_vecPerPixelY; //vector to add to a point on the view plane to get to ne next pixel along y
+    u32 m_maxBounces; //max bounces for each ray
 };
 
 } //scene
