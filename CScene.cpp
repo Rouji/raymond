05ab7e5e8983c7c7fdaeb9867c44c5fcdb382c7c
Andreas Hackl <andreas.hackl@johanniter.at>
2016-01-24 18:36:35 -0800

    added transmittance

--- CScene.cpp
+++ CScene.cpp
@@ -9,7 +9,7 @@ namespace scene
 {
 
 //distance to add between a surface and ray cast from it
-const f32 RECAST_MARGIN = FLOAT_ROUNDING_ERROR_32 * 2.0f; 
+const f32 RECAST_MARGIN = FLOAT_ROUNDING_ERROR_32 * 5.0f; 
 
 CScene::CScene() :
     m_pCamera(0),
@@ -62,7 +62,7 @@ u32 CScene::render()
         for (u32 x = 0; x < imageSize.X; ++x)
         {
             ray = m_pCamera->getRay(vec2i((s32)x, (s32)y));
-            pixelCol = trace(ray);
+            pixelCol = trace(ray,m_pCamera->getMaxBounces());
             writer.SetPixel(dim2i((s32)x, (s32)y), pixelCol);
         }
     }
@@ -97,8 +97,10 @@ void CScene::clear()
 
 col4f CScene::trace(const ray3f& ray, u32 depth)
 {
+    --depth;
     col4f overallCol(0.0f);
     SIntersection intersect;
+    vec3f interAddMargin;
     CSceneObject* pCurrObj = intersectScene(ray, &intersect);
 
     //no intersection found, return clear colour
@@ -113,20 +115,74 @@ col4f CScene::trace(const ray3f& ray, u32 depth)
     //return col4f(1.0f - (ray.getOrigin().distance(intersect.IntersectionPoint) / 10.0f),0.0f,0.0f); //render depth
 
     
+    //---shadow rays
     //iterate through light sources
+    interAddMargin = intersect.IntersectionPoint + (intersect.Normal * RECAST_MARGIN);//account for rounding errors
     for (std::list<CLight*>::iterator it = m_lightList.begin();
          it != m_lightList.end();
          it++)
     {
-        //account for rounding errors
-        intersect.IntersectionPoint += (intersect.Normal * RECAST_MARGIN);
-
-        if (!intersectShadow(intersect.IntersectionPoint, *it))
+        if (!intersectShadow(interAddMargin, *it))
         {
             overallCol += (*it)->shade(intersect.IntersectionPoint,
                                        intersect.Normal,
                                        m_pCamera->getEyePoint(),
                                        pCurrObj->Material);
+        }
+    }
+
+    //reached max. recursion depth 
+    if (depth == 0)
+        return overallCol;
+    
+    //lower to accommodate for reflectance etc.
+    overallCol *= 1.0f - pCurrObj->Material.Reflectance - pCurrObj->Material.Transmittance;
+
+    //---reflection
+    if (pCurrObj->Material.Reflectance > 0.0f)
+    {
+        vec3f reflDir = -(ray.getDirection().mirrorAlongNormal(intersect.Normal));
+        overallCol += trace(ray3f(intersect.IntersectionPoint, reflDir),depth) * pCurrObj->Material.Reflectance;
+    }
+
+    //---refraction
+    if (pCurrObj->Material.Transmittance > 0.0f && pCurrObj->Material.Refraction > 0.0f)
+    {
+        f32 n; //IOR ratio
+        f32 cosT;
+        vec3f outDir;
+        vec3f norm = intersect.Normal;
+        const vec3f& rayDir = ray.getDirection();
+        f32 cos = rayDir.dot(norm);
+
+        if (cos < 0) //air->obj
+        {
+            n = 1.0f/pCurrObj->Material.Refraction;
+            cos = -cos;
+        }
+        else //obj->air
+        {
+            n = pCurrObj->Material.Refraction;
+            
+            norm *= -1.0f;
+        }
+
+        cosT = 1.0f - maths::square(n) * (1.0f - maths::square(cos));
+        
+
+        //total internal refl.
+        if (cosT < 0.0f)
+        {
+            vec3f reflDir = -(ray.getDirection().mirrorAlongNormal(norm));
+            overallCol += trace(ray3f(intersect.IntersectionPoint, reflDir), depth) * pCurrObj->Material.Reflectance;
+        }
+        else
+        {
+            cosT = maths::squareroot(cosT);
+            outDir = rayDir * n + (norm * ((n * cos) - cosT));
+
+            interAddMargin = intersect.IntersectionPoint - (norm * RECAST_MARGIN);
+            overallCol += trace(ray3f(interAddMargin, outDir), depth) * pCurrObj->Material.Transmittance;
         }
     }
     
